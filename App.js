1c6d5de7987c1209edc2c58d3bf675f80fcdce66
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-12-17 22:22:09 -0300

    test: Relative position

--- App.js
+++ App.js
@@ -5,12 +5,23 @@ import {
   Animated,
   Button,
   Image,
-  Easing
+  Easing,
+  Dimensions
 } from 'react-native';
+
+import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
+
+const width = Math.round(Dimensions.get('window').width);
+const height = Math.round(Dimensions.get('window').height);
 
 export default function App() {
   const [posX] = useState(new Animated.Value(0.5));
   const [posY] = useState(new Animated.Value(1));
+
+  const ballSize = {
+    x: vw((64 * 100) / width),
+    y: vh((64 * 100) / height)
+  };
 
   function move(orientation) {
     const offsetX =
@@ -25,13 +36,13 @@ export default function App() {
       Animated.sequence([
         Animated.timing(posY, {
           toValue: 0,
-          duration: 200,
-          easing: Easing.bezier(0.42, 0, 1, 1)
+          duration: 160,
+          easing: Easing.ease.IN
         }),
         Animated.timing(posY, {
           toValue: 1,
-          duration: 200,
-          easing: Easing.bezier(0.42, 0, 1, 1)
+          duration: 160,
+          easing: Easing.ease.IN
         })
       ])
     ]).start();
@@ -69,16 +80,16 @@ export default function App() {
       >
         <Animated.View
           style={{
-            width: 64,
-            height: 64,
+            width: ballSize.x,
+            height: ballSize.y,
             position: 'absolute',
             backgroundColor: '#732881',
             alignSelf: 'center',
             top: posY.interpolate({
               inputRange: [0, 1],
-              outputRange: [50, -32]
+              outputRange: [50, -(ballSize.x / 2)]
             }),
-            borderRadius: 32
+            borderRadius: ballSize.x / 2
           }}
         />
         <Image
